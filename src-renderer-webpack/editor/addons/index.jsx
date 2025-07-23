1dde543cfa81b73ee2004e1d160d72e07da427bc
Marrcus113 <deepseek748@gmail.com>
2025-07-23 18:02:34 +0300

    Update index.jsx

--- src-renderer-webpack/editor/addons/index.jsx
+++ src-renderer-webpack/editor/addons/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import AddonSettings from 'scratch-gui/src/addons/settings/settings.jsx';
 import '../prompt/prompt.js';
 import ErrorContainerHOC from '../error/error-container-hoc.jsx';
+import '../extensions/telegram-bot-api.js';
+
 
 const handleExportSettings = (settings) => {
   AddonsPreload.exportSettings(JSON.stringify(settings));
